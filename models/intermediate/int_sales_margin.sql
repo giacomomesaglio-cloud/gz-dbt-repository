b9d158cedecc227cc8f75c99c2a43b8f748cca1c
Giacomo Mesaglio <giacomo.mesaglio@gmail.com>
2026-04-30 09:17:15 +0000

    Creation of the sales_margin table with the margin

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,2 +1,14 @@
 --margin = revenue - purchase_cost
 --purchase_cost = quantity * purchase_price
+SELECT 
+products_id,
+orders_id,
+date_date,
+revenue,
+quantity,
+purchase_price,
+ROUND((quantity*purchase_price),2) AS purchase_cost,
+ROUND(revenue-(quantity*purchase_price),2) AS margin
+FROM {{ ref('stg_raw__sales') }} AS s
+LEFT JOIN {{ ref('stg_raw__product') }} AS p
+USING (products_id)
